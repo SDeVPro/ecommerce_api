39cb41aaf5a68ea16b27ae855bff26a6e9c31af9
Sardor Abdulxamidov <web_software_developer@mail.ru>
2021-11-09 10:14:39 +0500

    adashganlar

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 import logging 
 import json
+from django.utils import translation
 import requests
 from django.core.cache import cache
 from django.conf import settings 
@@ -34,6 +35,7 @@ from .serializers import (
     CategoryListSerializer,
     ProductDetailSerializer,
     ProductIndexSerializer,
+    ProductSerializer,
     ProductViewsSerializer,
     ProductMiniSerializer,
     SerpyProductSerializer,
@@ -81,4 +83,150 @@ class ListProductView(viewsets.ModelViewSet):
         if User.objects.get(username="Sardor")!=self.get_object().seller:
             raise NotAcceptable(_("You don't own product"))
         return super(ListProductView,self).update(request,*args,**kwargs)
+
+
+class ProductDocumentView(DocumentViewSet):
+    document = ProductDocument
+    serializer_class = ProductDocumentSerializer
+    lookup_field = "id"
+    filter_backends = [
+        FilteringFilterBackend,
+        OrderingFilterBackend,
+        DefaultOrderingFilterBackend,
+        SearchFilterBackend,
+    ]        
+    search_fields = ("title")
+    filter_fields = {"title":"title.raw"}
+    ordering_fields = {"created":"created"}
+    queryset = Product.objects.all()
+
+class CategoryListAPIView(ListAPIView):
+    serializer_class = CategoryListSerializer 
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )   
+    search_fields = ("name",)
+    ordering_fields = ("created",)
+    filter_fields = ("created",)
+
+    @time_calculator
+    def time(self):
+        return 0
+    def get_queryset(self):
+        queryset = Category.objects.all()
+        self.time()
+        return queryset 
+
+class CategoryAPIView(RetrieveAPIView):
+    serializer_class = CategoryListSerializer
+    queryset = Category.objects.all()
+    def retrive(self,request,*args,**kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        data = {}
+        for k,v in serializer.data.items():
+            data[k]=translator.translate(str(v),dest="ar").text 
+        return Response(data)
+class ListProductAPIView(ListAPIView):
+    serializer_class = ProductSerializer
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )    
+    search_fieds = ("title",)
+    ordering_fields = ("created",)
+    filter_fields = ("views",)
+    queryset = Product.objects.all()
+    @time_calculator
+    def time(self):
+        return 0
+    @method_decorator(cache_page(60*60*2))
+    @method_decorator(vary_on_cookie)
+    def list(self,request,*args,**kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page,many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset,many=True)
+        self.time()
+        return Response(serializer.data)
+class ListUserProductAPIView(ListAPIView):
+    serializer_class = ProductSerializer
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )  
+    search_fields = (
+        "title",
+        "user__username",
+    )
+    ordering_fields = ("created",)
+    filter_fields = ("views",)
+    def get_queryset(self):
+        user = self.request.user 
+        queryset = Product.objects.filter(user=user)
+        return queryset
+    
+class CreateProductAPIView(CreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = CreateProductSerializer
+    def create(self,request,*args,**kwargs):
+        user = request.user 
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(seller=user)
+        push_notifications(user,request.data["title"],"you have add a new product")
+        logger.info(
+            "product ("
+            +str(serializer.data.get("title"))
+            +") created"
+            + "by ( "
+            +str(user.username)
+            + " )"
+        )
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+class DestroyProductAPIView(DestroyAPIView):
+    permission_classes = [IsOwnerAuth]
+    serializer_class = ProductDetailSerializer 
+    queryset = Product.objects.all()
+    def destroy(self,request,*args,**kwargs):
+        instance = self.get_object()
+        instance.is_deleted = True 
+        instance.save()
+        return Response({"detail":"Product deleted"})
+class ProductViewAPIView(ListAPIView):
+    serializer_class = ProductViewsSerializer
+    queryset = ProductViews.objects.all()
+
+class ProductDetailView(APIView):
+    def get(self,request,uuid):
+        product = Product.objects.get(uuid=uuid)
+        x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(",")[0]
+        else:
+            ip = request.META.get("REMOTE_ADDR")
+        if not ProductViews.objects.filter(product=product,ip=ip).exists():
+            ProductViews.objects.create(product=product,ip=ip)
+            product.views += 1
+            product.save()
+        serializer = ProductDetailSerializer(product,context={"request":request})
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    def put(self,request,pk):
+        user = request.user 
+        product = get_object_or_404(Product,pk=pk)
+        if product.user != user:
+            serializer = ProductDetailSerializer(
+                product,data=request.data,context={"request":request}
+            )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
         
+
